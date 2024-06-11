b40e6a1a1d07639f2e32e74f2e5caa8c2cdc55ef
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-06-11 15:50:50 -0300

    atualizando repository

--- src/main/java/com/example/to_do_list/Repositories/TarefaRepository.java
+++ src/main/java/com/example/to_do_list/Repositories/TarefaRepository.java
@@ -7,6 +7,6 @@ import org.springframework.stereotype.Repository;
 import java.util.UUID;
 
 @Repository
-public interface TarefaRepository extends JpaRepository<Tarefa, UUID> {
+public interface TarefaRepository extends JpaRepository<Tarefa, Long> {
 
 }
