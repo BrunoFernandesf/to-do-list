277df3896ebb9dff3ed255bea1162416e3fc9d1c
BrunoFernandesf <brunofernandesf.gd@gmail.com>
2024-06-13 09:33:52 -0300

    adicionando TarefaController

--- src/main/java/com/example/to_do_list/Controllers/TarefaController.java
+++ src/main/java/com/example/to_do_list/Controllers/TarefaController.java
@@ -1,9 +1,42 @@
 package com.example.to_do_list.Controllers;
 
+import com.example.to_do_list.Entity.Tarefa;
 import com.example.to_do_list.Repositories.TarefaRepository;
+import com.example.to_do_list.Services.TarefaService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
 
+@RestController
+@RequestMapping("/tarefas")
 public class TarefaController {
+
+    private TarefaService tarefaService;
+
+    public TarefaController(TarefaService tarefaService){
+        this.tarefaService = tarefaService;
+    }
+
+    @PostMapping
+    List<Tarefa> create(@RequestBody Tarefa tarefa){
+        return tarefaService.create(tarefa);
+    }
+
+    @GetMapping
+    List<Tarefa> list(){
+        return tarefaService.list();
+    }
+
+    @PutMapping
+    List<Tarefa> update(@RequestBody Tarefa tarefa){
+        return tarefaService.update(tarefa);
+    }
+
+    @DeleteMapping("{id}")
+    List<Tarefa> delete(@PathVariable("id") Long id){
+        return tarefaService.delete(id);
+    }
+
 }
